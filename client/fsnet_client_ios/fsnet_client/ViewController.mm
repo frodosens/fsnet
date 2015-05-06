72a87d5b6d36a7cfd44804cb185c8d46d3feca9b
Frodo <frodosens@gmail.com>
2015-05-06 14:06:56 +0800

    去除一些多余的文件

--- client/fsnet_client_ios/fsnet_client/ViewController.mm
+++ client/fsnet_client_ios/fsnet_client/ViewController.mm
@@ -13,7 +13,6 @@
 #include "LoginChannel.h"
 
 
-static FSClientImpl* s_client;
 @implementation ViewController
 
 - (void)viewDidLoad {
@@ -23,14 +22,16 @@ static FSClientImpl* s_client;
     
     FACTORY_BIND_CHANNEL(LoginChannel);
     
-    s_client = new FSClientImpl("127.0.0.1", 40000);
-    if(s_client->connect()){
-        while (true) {
-            s_client->tick(0);
+    const int client_count = 1;
+    FSClientImpl* clients[client_count];
+    for(int i = 0 ; i < client_count ; i++){
+        clients[i] = new FSClientImpl("127.0.0.1", 40000);
+        clients[i]->connect();
+    }
+    while (true) {
+        for(int i = 0 ;i  <client_count ; i++){
+            clients[i]->tick(0);
         }
-    }else{
-        UIAlertView* view = [[UIAlertView alloc] initWithTitle:@"" message:@"连接失败" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
-        [view show];
     }
 }
 
